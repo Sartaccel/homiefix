1511f5a8fb4cf7fc279f7d2a591acc7485445a77
Jijonadarajan <jijonadarajan0701@gmail.com>
2024-11-19 10:44:07 +0530

    second commit

--- lib/presentation/widgets/bottom_navigation_bar.dart
+++ lib/presentation/widgets/bottom_navigation_bar.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:homiefix_application/presentation/constants/static.constants.dart';
 
 class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _BottomNavigationState createState() => _BottomNavigationState();
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
   int _selectedIndex = 0;
-  PageController _pageController = PageController();
+  final PageController _pageController = PageController();
 
   void _onItemTapped(int index) {
     setState(() {
@@ -23,7 +26,11 @@ class _BottomNavigationState extends State<BottomNavigation> {
     });
   }
 
-  BottomNavigationBarItem _buildBottomNavigationBarItem(IconData outlinedIcon, IconData filledIcon, String label, int index) {
+  BottomNavigationBarItem _buildBottomNavigationBarItem(
+    IconData outlinedIcon, 
+    IconData filledIcon, 
+    String label, 
+    int index) {
     return BottomNavigationBarItem(
       icon: Icon(_selectedIndex == index ? filledIcon : outlinedIcon),
       label: label,
@@ -36,23 +43,29 @@ class _BottomNavigationState extends State<BottomNavigation> {
       body: PageView(
         controller: _pageController,
         onPageChanged: _onPageChanged,
-        children: <Widget>[
+        children: const <Widget>[
           Center(child: Text('Home')),
           Center(child: Text('Search')),
           Center(child: Text('Profile')),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          _buildBottomNavigationBarItem(Icons.home_outlined, Icons.home, Constants.homeNavi, 0),
-          _buildBottomNavigationBarItem(Icons.shopping_cart_outlined, Icons.shopping_cart, Constants.cartNavi, 1),
-          _buildBottomNavigationBarItem(Icons.person_outline_rounded, Icons.person, Constants.profileNavi, 2),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.black,
-        onTap: _onItemTapped,
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          splashFactory: NoSplash.splashFactory, // Removes ripple effect
+          highlightColor: Colors.transparent,   // Removes highlight effect
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            _buildBottomNavigationBarItem(Icons.home_outlined, Icons.home, Constants.homeNavi, 0),
+            _buildBottomNavigationBarItem(Icons.shopping_cart_outlined, Icons.shopping_cart, Constants.cartNavi, 1),
+            _buildBottomNavigationBarItem(Icons.person_outline_rounded, Icons.person, Constants.profileNavi, 2),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.green,
+          unselectedItemColor: Colors.black,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
